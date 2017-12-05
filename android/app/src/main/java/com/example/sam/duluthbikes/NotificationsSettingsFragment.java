dae348975ddcb50d68de658d7072f2342a82ec5a
gilbe935@d.umn.edu <gilbe935@d.umn.edu>
2017-12-05 09:15:42 -0600

    Further implemented NotificationsSettingsFragment:  created and set private variables, set notification toggle button behavior

--- android/app/src/main/java/com/example/sam/duluthbikes/NotificationsSettingsFragment.java
+++ android/app/src/main/java/com/example/sam/duluthbikes/NotificationsSettingsFragment.java
@@ -12,20 +12,61 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TimePicker;
+import android.widget.ToggleButton;
 
 /**
  * Created by Mackenzie Fulton on 11/16/2017.
+ *
+ * Class to handle the the inner workings of the Notifications menu
  */
 
 public class NotificationsSettingsFragment extends Fragment {
 
-    View myView;
+    // Private variables
+    private View myView;
+    private ToggleButton toggleNotifications;
+    private TimePicker timePicker;
+    private Button setTime;
 
     @Nullable
     @Override
+    /* Class ctor
+    Opens the layout, sets certain layout elements to variables, defines button click
+    response, determines if toggle is active or inactive
+     */
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.activity_notifications, container, false);
 
+        toggleNotifications = (ToggleButton) myView.findViewById(R.id.toggle_notifications);
+
+        timePicker = (TimePicker) myView.findViewById(R.id.time_picker);
+
+        setTime = (Button) myView.findViewById(R.id.set_time);
+
+        determineSetTimeState();
+
+        toggleNotifications.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                determineSetTimeState();
+            }
+        });
+
         return myView;
+    }
+
+    /* Determines if the notification toggle button is on or off; allowing or
+    disallowing the user to press the "set" button
+     */
+    private void determineSetTimeState() {
+        if (!toggleNotifications.isChecked()) {
+            setTime.setEnabled(false);
+            timePicker.setEnabled(false);
+        } else {
+            setTime.setEnabled(true);
+            timePicker.setEnabled(true);
+        }
     }
 }
