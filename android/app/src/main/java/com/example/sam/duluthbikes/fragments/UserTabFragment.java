ea8f2941be139b8de78eab85615f84bd7be205b1
Craig Rudrud <rudru017@d.umn.edu>
2018-03-21 20:57:19 -0500

    Added erase button to ride history screen

--- android/app/src/main/java/com/example/sam/duluthbikes/fragments/UserTabFragment.java
+++ android/app/src/main/java/com/example/sam/duluthbikes/fragments/UserTabFragment.java
@@ -1,5 +1,7 @@
 package com.example.sam.duluthbikes.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,10 +9,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sam.duluthbikes.R;
 import com.example.sam.duluthbikes.UnitConverter;
+
+import org.w3c.dom.Text;
 
 import java.text.DecimalFormat;
 
@@ -24,15 +30,18 @@ public class UserTabFragment extends Fragment {
     TextView totalDist;
     TextView avgDist;
     TextView totalTime;
+    TextView avgTime;
     TextView rideData;
     Float totDistance;
     Float avgDistance;
     Long totTime;
+    Long averageTime;
     int numberOfRides;
     UnitConverter converter;
     DecimalFormat df;
+    Button eraseAllRides;
 
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.activity_user_tab, container, false);
 
         converter = new UnitConverter();
@@ -40,6 +49,7 @@ public class UserTabFragment extends Fragment {
         totalDist = (TextView)myView.findViewById(R.id.homeTotalDistance);
         avgDist = (TextView)myView.findViewById(R.id.homeAvgDistance);
         totalTime = (TextView)myView.findViewById(R.id.homeTotalTime);
+        avgTime = (TextView)myView.findViewById(R.id.homeAvgTime);
         rideData = (TextView)myView.findViewById(R.id.allRideDataGoesHere);
 
         df = new DecimalFormat("#.##");
@@ -47,10 +57,20 @@ public class UserTabFragment extends Fragment {
         initializeTotals();
 
         totalDist.setText(df.format(totDistance.doubleValue()/1000).toString() + " km");
-        avgDist.setText(df.format((totDistance.doubleValue()/1000)/numberOfRides).toString() + " km");
         totalTime.setText(converter.convertHoursMinSecToString(totTime));
 
         retrieveRideData();
+
+        if(numberOfRides != 0) {
+            avgDist.setText(df.format((totDistance.doubleValue() / 1000) / numberOfRides).toString() + " km");
+            avgTime.setText(converter.convertHoursMinSecToString(totTime/numberOfRides));
+        }
+        else {
+            avgDist.setText("0 km");
+            avgTime.setText(converter.convertHoursMinSecToString(totTime));
+        }
+
+        eraseAllRides = makeEraseAllRidesButton();
 
         return myView;
     }
@@ -62,6 +82,7 @@ public class UserTabFragment extends Fragment {
         totDistance = totalstats.getFloat(getString(R.string.lifetimeStats_totDist), 0);
         avgDistance = totalstats.getFloat(getString(R.string.lifetimeStats_avgDist), 0);
         totTime = totalstats.getLong(getString(R.string.lifetimeStats_totTime), 0);
+        averageTime = totalstats.getLong(getString(R.string.lifetimeStats_avgDist), 0);
         numberOfRides = totalstats.getInt(getString(R.string.lifetimeStats_rideNumber), 0);
 
     }
@@ -69,6 +90,10 @@ public class UserTabFragment extends Fragment {
     private void retrieveRideData(){
 
         SharedPreferences totalstats = getActivity().getSharedPreferences(getString(R.string.lifetimeStats_file_key), 0);
+
+        if(numberOfRides == 0) {
+            rideData.append("No rides!");
+        }
 
         for (int num = numberOfRides; num >= 1;  num--){
             String rideTime = "ride" + num + "Time";
@@ -83,5 +108,45 @@ public class UserTabFragment extends Fragment {
                     "Time: " + converter.convertHoursMinSecToString(rideT) + "\n\n");
         }
 
+    }
+
+    private void eraseAllRides() {
+
+        SharedPreferences totalStats = getActivity()
+                .getSharedPreferences(getString(R.string.lifetimeStats_file_key), 0);
+        totalStats.edit().clear().commit();
+        getFragmentManager().beginTransaction().detach(this).attach(this).commit();
+    }
+
+    private Button makeEraseAllRidesButton() {
+
+        Button button = (Button)myView.findViewById(R.id.button_eraseAllRides);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogInterface.OnClickListener dialogBox = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int choice) {
+                        switch (choice) {
+                            case DialogInterface.BUTTON_POSITIVE:
+                                eraseAllRides();
+                                Toast.makeText(getActivity().getApplicationContext(),
+                                        getString(R.string.eraseAllRidesToast), Toast.LENGTH_SHORT)
+                                        .show();
+                                break;
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                break;
+                        }
+                    }
+                };
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(myView.getContext());
+                builder.setMessage(getString(R.string.eraseAllRidesPopup))
+                        .setPositiveButton("Yes", dialogBox)
+                        .setNegativeButton("No", dialogBox).show();
+            }
+        });
+
+        return button;
     }
 }
