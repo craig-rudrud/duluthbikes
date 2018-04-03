1def03d781de3af159d9ebafd60da34fe9a64d26
Evan Composto <compo010@d.umn.edu>
2018-04-02 21:24:45 -0500

    Changes to server

--- node/index.js
+++ node/index.js
@@ -21,14 +21,14 @@ var io = require('socket.io')(http);
 var routeHistory = [];
 
 // setting the port for the app system to use
-app.set("port",23401);
+app.set("port",23405);
 
 // this section tells the body parser what type of data to expect
 // for now it is mainly json
 // Set limits in order to send images.
 app.use(bodyParser.urlencoded({
-        limit: '50mb',
-        extended: true
+    limit: '50mb',
+    extended: true
 }));
 app.use(bodyParser.json({limit: '50mb'}));
 
@@ -41,33 +41,33 @@ var mongodb = require('./mongoDB.js')();
 console.log(mongodb);
 
 app.get('/heatmapfiles',function(req,res){
-	res.sendFile(__dirname + '/public/node_modules/heatmap.js/build/heatmap.js');
+    res.sendFile(__dirname + '/public/node_modules/heatmap.js/build/heatmap.js');
 });
 
 app.get('/heatmapfilesgmaps',function(req,res){
-  res.sendFile(__dirname + '/public/node_modules/heatmap.js/plugins/gmaps-heatmap/gmaps-heatmap.js');
+    res.sendFile(__dirname + '/public/node_modules/heatmap.js/plugins/gmaps-heatmap/gmaps-heatmap.js');
 });
 
 app.get('/fullRide',function(req,res){
-	var rides = printRides('FullRidesRecorded',function(result){
-		res.write('<HTML><head><title>Duluth Bikes DashBoard</title></head><BODY>'
-		+'<H1>Full Rides.</H1>');
-		result.reverse();
-		res.write(JSON.stringify(result));
-		res.send();
-	});
-	console.log('full ride request');
+    var rides = printRides('FullRidesRecorded',function(result){
+	res.write('<HTML><head><title>Duluth Bikes DashBoard</title></head><BODY>'
+		  +'<H1>Full Rides.</H1>');
+	result.reverse();
+	res.write(JSON.stringify(result));
+	res.send();
+    });
+    console.log('full ride request');
 }); 
 
 app.get('/fulllatlng',function(req,res){
-        var rides = printRides('FullLatLngsRecorded',function(result){
-                res.write('<HTML><head><title>Duluth Bikes DashBoard</title></head><BODY>'
-                +'<H1>Full Rides.</H1>');
-                result.reverse();
-                res.write(JSON.stringify(result));
-                res.send();
-        });
-        console.log('full ride request');
+    var rides = printRides('FullLatLngsRecorded',function(result){
+        res.write('<HTML><head><title>Duluth Bikes DashBoard</title></head><BODY>'
+                  +'<H1>Full Rides.</H1>');
+        result.reverse();
+        res.write(JSON.stringify(result));
+        res.send();
+    });
+    console.log('full ride request');
 });
 
 // this next section is for our GET, POST, PUT, DELETE routes
@@ -75,166 +75,188 @@ app.get('/fulllatlng',function(req,res){
 //
 app.get('/raw', function(request, response) {
 
-	//when using Mongo
-	var str = printDatabase('RideHistory', function(result) {
-		response.write('<HTML><head><title>Duluth Bikes DashBoard</title></head><BODY>' 
-		+ '<H1>Duluth Bikes Route Data.</H1>')
-		result.reverse();
-		response.write(JSON.stringify(result,['point','lat','lng'],'\n') + '</BODY></HTML>');
-		response.send();
-    	});
-	
-	console.log('DashBoard request received!');
+    //when using Mongo
+    var str = printDatabase('RideHistory', function(result) {
+	response.write('<HTML><head><title>Duluth Bikes DashBoard</title></head><BODY>' 
+		       + '<H1>Duluth Bikes Route Data.</H1>')
+	result.reverse();
+	response.write(JSON.stringify(result,['point','lat','lng'],'\n') + '</BODY></HTML>');
+	response.send();
+    });
+    
+    console.log('DashBoard request received!');
 });
 
 app.get('/rides',function(request,response){
-	response.sendFile(__dirname +'/public/ride.html');
-	printRides('FullRidesRecorded',function(doc){
+    response.sendFile(__dirname +'/public/ride.html');
+    printRides('FullRidesRecorded',function(doc){
         io.emit('FullRidesRecorded',doc);
-        });
+    });
 });
 
 app.get('/maps',function(req,res){
-	res.sendFile(__dirname + '/public/maps.html');
-	printRides('FullRidesRecorded',function(doc){
+    res.sendFile(__dirname + '/public/maps.html');
+    printRides('FullRidesRecorded',function(doc){
 	io.emit('FullRidesRecorded',doc);
-	});
+    });
 });
 
 app.get('/',function(req,res){
-	res.sendFile(__dirname + '/public/duluthBikesBootstrap.html');
+    res.sendFile(__dirname + '/public/duluthBikesBootstrap.html');
 });
 
 app.post('/postlocalleaderboard', function(request,response) {
 
-	if (!request.body) return response.sendStatus(400);
+    if (!request.body) return response.sendStatus(400);
 
-	var position = {
-		'pos':request.body.pos
-	}
-	var statData = {
-		'date':request.body.date,
-		'distance':request.body.distance,
-		'time':request.body.time,
-		'name':request.body.name
-	}
-	insertLocalLeaderboard(position,statData);
-	console.log('Post Request: postlocalleaderboard');
-	response.sendStatus(200);
+    var position = {
+	'pos':request.body.pos
+    }
+    var statData = {
+	'date':request.body.date,
+	'distance':request.body.distance,
+	'time':request.body.time,
+	'name':request.body.name
+    }
+    insertLocalLeaderboard(position,statData);
+    console.log('Post Request: postlocalleaderboard');
+    response.sendStatus(200);
 });
 
 app.post('/postgloballeaderboard', function(request,response) {
-	
-		if (!request.body) return response.sendStatus(400);
-	
-		var position = {
-			'pos':request.body.pos
-		}
-		var statData = {
-			'date':request.body.date,
-			'distance':request.body.distance,
-			'time':request.body.time,
-			'name':request.body.name
-		}
-		insertGlobalLeaderboard(position,statData);
-		console.log('Post Request: postgloballeaderboard');
-		response.sendStatus(200);
-	});
+    
+    if (!request.body) return response.sendStatus(400);
+    
+    var position = {
+	'pos':request.body.pos
+    }
+    var statData = {
+	'date':request.body.date,
+	'distance':request.body.distance,
+	'time':request.body.time,
+	'name':request.body.name
+    }
+    insertGlobalLeaderboard(position,statData);
+    console.log('Post Request: postgloballeaderboard');
+    response.sendStatus(200);
+});
 
 app.post('/postroute', function(request, response) {
 
-	if (!request.body)return response.sendStatus(400);
+    if (!request.body)return response.sendStatus(400);
 
-	var routeData = {'lat':request.body.lat,
-			 'lng':request.body.lang};
+    var routeData = {'lat':request.body.lat,
+		     'lng':request.body.lang};
 
-	var date = {'Date':request.body.time};
+    var date = {'Date':request.body.time};
 
-	//Mongo
-	insertRoute(routeData);
+    //Mongo
+    insertRoute(routeData);
 
-	console.log('Post Request: postroute');
+    console.log('Post Request: postroute');
 
-	response.sendStatus(200);
+    response.sendStatus(200);
 });
 
 app.post('/postfinish',function(req,res){
 
-	if(!req.body)return res.sendStatus(400);	
+    if(!req.body)return res.sendStatus(400);	
 
-	var arr = [];
-	arr = req.body.ride;
-	insertFullRide(arr);
-	if(req.body.heat){
+    var arr = [];
+    arr = req.body.ride;
+    insertFullRide(arr);
+    if(req.body.heat){
 	var latlng = [];
 	latlng = req.body.heat;
 	insertLatLng(latlng);
 	
 	io.emit('FullRidesRecorded',doc);
-	}
-	console.log('Post Full Ride');
+    }
+    console.log('Post Full Ride');
 
-	res.sendStatus(200);
+    res.sendStatus(200);
 });
 
 
 app.get('/usernames', function(req,res){
-	var users = printUsers('UsersSaved',function(result){
-         res.write('<HTML><head><title>Duluth Bikes DashBoard</title></head><BODY>'
-            +'<H1>Users </H1>');
-            res.write(JSON.stringify(result));
-            res.send();
-        });
+    var users = printUsers('UsersSaved',function(result){
+        res.write('<HTML><head><title>Duluth Bikes DashBoard</title></head><BODY>'
+		  +'<H1>Users </H1>');
+        res.write(JSON.stringify(result));
+        res.send();
+    });
     console.log('users request');
 });
 
 app.get('/localleaderboard', function(req, res) {
-	var users = printLocalLeaderboard('localLeaderboard',function(result){
-		   res.write(JSON.stringify(result));
-		   res.send();
-	   });
-   console.log('local leaderboard request');
+    var users = printLocalLeaderboard('localLeaderboard',function(result){
+	res.write(JSON.stringify(result));
+	res.send();
+    });
+    console.log('local leaderboard request');
 });
 
 app.get('/globalleaderboard', function(req, res) {
-	var users = printLocalLeaderboard('globalLeaderboard',function(result){
-		   res.write(JSON.stringify(result));
-		   res.send();
-	   });
-   console.log('global leaderboard request');
+    var users = printLocalLeaderboard('globalLeaderboard',function(result){
+	res.write(JSON.stringify(result));
+	res.send();
+    });
+    console.log('global leaderboard request');
 });
 
-app.post('/postusername', function(req,res){
-	if(!req.body)return res.sendStatus(400);
-	var userObj = { 'user':req.body.userName,
-					'pass':req.body.passWord };
-	insertUsername(userObj);
-	console.log('Post Username');
-	res.send('good');
+app.post('/loginAttempt', function(req,res){
+    if(!req.body.userName || !req.body.passWord) return res.sendStatus(400);
+    var userObj = { 'user':req.body.userName,
+		    'pass':req.body.passWord };
+    console.log('loginAttempt');
+    if(checkCreds(userObj))
+    {   console.log("wtf")
+	res.sendStatus(200);
+    }
+    else {
+	res.sendStatus(300);
+	console.log("help")
+    }
+    console.log("success")
+});
 
+app.post('/newAccount', function(req,res){
+    if(!req.body) return res.sendStatus(400);
+    var userObj = { 'user':req.body.userName,
+		    'pass':req.body.passWord,
+		    'email':req.body.email};
+    if(newAccount(userObj))
+    {   console.log("wtf")
+	res.sendStatus(200);
+    }
+    else {
+	res.sendStatus(300);
+	console.log("help")
+    }
+    console.log("success")
 });
 
 app.post('/postpicture', function(req,res){
-	if(!req.body)return res.sendStatus(400);
-	var picObj = { 'location':req.body.loc,
-		       'description':req.body.description,
-		       'picture':req.body.picture };
-	insertPicture(picObj);
-	console.log('Post Picture');
-	res.send();
+    if(!req.body.userName || !req.body.passWord) return res.sendStatus(400);
+    var picObj = { 'location':req.body.loc,
+		   'description':req.body.description,
+		   'picture':req.body.picture };
+    insertPicture(picObj);
+    console.log('Post Picture');
+    res.send();
 
 });
 
 app.get('/pictures',function(req,res){
-	
-	// 1.// THE FOLLOWING IS FOR ACCESSING DB. ( CURRENTLY DOES NOT ACCESS - PICS HARDCODED.)
-	res.sendFile(__dirname +'/public/threepics.html'); // Will try and use if we can use Canvas element - HTML5
-	printPictures('PicturesSaved',function(doc){
+    
+    // 1.// THE FOLLOWING IS FOR ACCESSING DB. ( CURRENTLY DOES NOT ACCESS - PICS HARDCODED.)
+    res.sendFile(__dirname +'/public/threepics.html'); // Will try and use if we can use Canvas element - HTML5
+    printPictures('PicturesSaved',function(doc){
 	io.emit('PicturesSaved',doc);
-	});
+    });
 
-	// 2.// THE FOLLOWING WILL PRINT THE RAW PICTURE DATA STORED IN DB
-	//var pics = printPictures('PicturesSaved',function(result){
+    // 2.// THE FOLLOWING WILL PRINT THE RAW PICTURE DATA STORED IN DB
+    //var pics = printPictures('PicturesSaved',function(result){
     //    res.write('<HTML><head><title>Duluth Bikes DashBoard</title></head><BODY>'
     //        +'<H1>Pictures.</H1>');
     //        res.write(JSON.stringify(result));
@@ -244,62 +266,71 @@ app.get('/pictures',function(req,res){
 });
 
 app.get('/deleteallthepictures',function(res,req) {
-	console.log('deleted all pictures attempt');
+    console.log('deleted all pictures attempt');
 
-	deleteAll('PicturesSaved', function(result) {
-		if(result==true)console.log("deleted all pictures");
-		else console.log("Did not work");
-	});
+    deleteAll('PicturesSaved', function(result) {
+	if(result==true)console.log("deleted all pictures");
+	else console.log("Did not work");
+    });
 });
 
 
 app.get('/deletealltherides',function(res,req){
-	console.log('deleted all rides atempt');
-	
-	deleteAll('FullLatLngsRecorded',function(result){
-		if(result==true)console.log("deleted all");
-		else console.log("didnt work");
-		});
+    console.log('deleted all rides atempt');
+    
+    deleteAll('FullLatLngsRecorded',function(result){
+	if(result==true)console.log("deleted all");
+	else console.log("didnt work");
+    });
 });
 
 app.get('/deleteAllUsers',function(res,req){
-	console.log('deleted all users atempt');
-	
-	deleteAll('UsersSaved',function(result){
-		if(result==true)console.log("deleted all");
-		else console.log("didnt work");
-		});
+    console.log('deleted all users atempt');
+    
+    deleteAll('UsersSaved',function(result){
+	if(result==true)console.log("deleted all");
+	else console.log("didnt work");
+    });
 });
 
 app.get('/resetLocalLeaderboard',function(res,req){
-	console.log('deleted all local leaderboard atempt');
-	
-	deleteAll('localLeaderboard',function(result){
-		if(result==true)console.log("deleted all");
-		else console.log("didnt work");
-		});
+    console.log('deleted all local leaderboard atempt');
+    
+    deleteAll('localLeaderboard',function(result){
+	if(result==true)console.log("deleted all");
+	else console.log("didnt work");
+    });
 });
 
 app.get('/resetGlobalLeaderboard',function(res,req){
-	console.log('deleted all global leaderboard atempt');
-	
-	deleteAll('globalLeaderboard',function(result){
-		if(result==true)console.log("deleted all");
-		else console.log("didnt work");
-		});
+    console.log('deleted all global leaderboard atempt');
+    
+    deleteAll('globalLeaderboard',function(result){
+	if(result==true)console.log("deleted all");
+	else console.log("didnt work");
+    });
 });
 
-io.on('connection',function(socket){
-	console.log('a socket io connection');
 
-	printRides('FullRidesRecorded',function(doc){
+
+
+io.on('connection',function(socket){
+    console.log('a socket io connection');
+
+    printRides('FullRidesRecorded',function(doc){
 	socket.emit('FullRidesRecorded',doc);
-	});
+    });
 });
 
 function convertBase64ToImage(){
 
 }
+
+app.get('/logout', (req, res)=>{
+    res.write("logout");
+});
+
+
 
 // this last section is to start the app and start listening on 
 // the given port for requests
@@ -308,7 +339,7 @@ function convertBase64ToImage(){
 
 
 http.listen(app.get("port"),function(){
-	console.log('duluth bikes node listening on port:',app.get("port"));
+    console.log('duluth bikes node listening on port:',app.get("port"));
 });
 
 
--- node/mongoDB.js
+++ node/mongoDB.js
@@ -1,8 +1,8 @@
 
 //database url
 //var url = 'mongodb://127.0.0.1:50432/db'; 
-var url = 'mongodb://127.0.0.1:27017/db';
-var collections = ['rides'];
+var url = 'mongodb://127.0.0.1:23406/db';
+var collections = ['rides', 'users', 'RideHistory', 'FullRidesRecorded'];
 
 var mongojs = require('mongojs');
 var assert = require('assert');
@@ -10,198 +10,202 @@ var assert = require('assert');
 console.log("MongoDB is active.");
 
 module.exports = function () {
-	var mongodb = mongojs(url, collections); //creation of the mongo connection
+    var mongodb = mongojs(url, collections); //creation of the mongo connection
 
-	mongodb.on('error', function (err) {
-		console.log('database error', err)
-	})
+    mongodb.on('error', function (err) {
+	console.log('database error', err)
+    })
 
-	mongodb.on('connect', function () {
-		console.log('database connected DulBik')
-	})
+    mongodb.on('connect', function () {
+	console.log('database connected DulBik')
+    })
 
-	/** ********************************************************************
-     	* printDatabase - Prints the whole collection, for debugging purposes.
-    	* @param collectionName - the name of the collection
-     	* @param callback - need to provide a function to return the data
-     	*/
-	printDatabase = function (collectionName, callback) {
+    /** ********************************************************************
+     * printDatabase - Prints the whole collection, for debugging purposes.
+     * @param collectionName - the name of the collection
+     * @param callback - need to provide a function to return the data
+     */
+    printDatabase = function (collectionName, callback) {
 
-		// 
-		// Collection look ups with find return a 
-		// MongoDB 'cursor'. More info can be found here
-		// https://docs.mongodb.com/v3.2/reference/glossary/#term-cursor
-		// 
+	// 
+	// Collection look ups with find return a 
+	// MongoDB 'cursor'. More info can be found here
+	// https://docs.mongodb.com/v3.2/reference/glossary/#term-cursor
+	// 
 
-		var cursor = mongodb.collection(collectionName).find(function (err, docs) {
+	var cursor = mongodb.collection(collectionName).find(function (err, docs) {
 
-			if (err || !docs) {
-				console.log("Cannot print database or database is empty\n");
-			}
-			else {
-				//console.log(collectionName, docs);
+	    if (err || !docs) {
+		console.log("Cannot print database or database is empty\n");
+	    }
+	    else {
+		//console.log(collectionName, docs);
 
-				callback(docs);
-			}
-		});
+		callback(docs);
+	    }
+	});
 
-	};
+    };
 
-	printRides = function (colName, callback) {
+    printRides = function (colName, callback) {
 
-		var cursor = mongodb.collection(colName).find(function (err, docs) {
+	var cursor = mongodb.collection(colName).find(function (err, docs) {
 
-			if (err || !docs) {
-				console.log("database empty\n");
-			}
-			else {
-				callback(docs);
-			}
-		});
-	};
+	    if (err || !docs) {
+		console.log("database empty\n");
+	    }
+	    else {
+		callback(docs);
+	    }
+	});
+    };
 
-	insertRoute = function (routeData) {
-		mongodb.collection('RideHistory').save(
-			{ point: routeData }, function (err, result) {
-				if (err || !result) console.log("point not saved");
-				else console.log("point entered into RideHistory");
-			});
-	};
+    insertRoute = function (routeData) {
+	mongodb.collection('RideHistory').save(
+	    { point: routeData }, function (err, result) {
+		if (err || !result) console.log("point not saved");
+		else console.log("point entered into RideHistory");
+	    });
+    };
 
-	insertFullRide = function (fullRide) {
-		mongodb.collection('FullRidesRecorded').save(
-			{ ride: fullRide }, function (err, result) {
-				if (err || !result) console.log("ride not saved");
-				else console.log("ride loged in FullRidesRecord");
-			});
-	};
+    insertFullRide = function (fullRide) {
+	mongodb.collection('FullRidesRecorded').save(
+	    { ride: fullRide }, function (err, result) {
+		if (err || !result) console.log("ride not saved");
+		else console.log("ride loged in FullRidesRecord");
+	    });
+    };
 
-	insertLocalLeaderboard = function (pos, stats) {
-		var posInt = pos.pos //pos is an array of key value pairs, pos is the key value for the position
-		if (posInt >= 1 && posInt <= 3) {
-			mongodb.collection('localLeaderboard').update(
-				{ position: pos }, //'find' query
-				{
-					position: pos,	//update these (or insert)
-					date: stats.date,
-					time: stats.time,
-					distance: stats.distance,
-					name: stats.name
-				},
-				{ upsert: true } //insert if not found
-			 )
-		}
-		else {
-			console.log('invalid pos passed to insertLocalLeaderboard!');
-		}
+    insertLocalLeaderboard = function (pos, stats) {
+	var posInt = pos.pos //pos is an array of key value pairs, pos is the key value for the position
+	if (posInt >= 1 && posInt <= 3) {
+	    mongodb.collection('localLeaderboard').update(
+		{ position: pos }, //'find' query
+		{
+		    position: pos,	//update these (or insert)
+		    date: stats.date,
+		    time: stats.time,
+		    distance: stats.distance,
+		    name: stats.name
+		},
+		{ upsert: true } //insert if not found
+	    )
 	}
-
-	insertGlobalLeaderboard = function (pos, stats) {
-		var posInt = pos.pos //pos is an array of key value pairs, pos is the key value for the position
-		if (posInt >= 1 && posInt <= 3) {
-			mongodb.collection('globalLeaderboard').update(
-				{ position: pos }, //'find' query
-				{
-					position: pos,	//update these (or insert)
-					date: stats.date,
-					time: stats.time,
-					distance: stats.distance,
-					name: stats.name
-				},
-				{ upsert: true } //insert if not found
-			 )
-		}
-		else {
-			console.log('invalid pos passed to insertGlobalLeaderboard!');
-		}
+	else {
+	    console.log('invalid pos passed to insertLocalLeaderboard!');
 	}
+    }
 
-	insertLatLng = function (LatLng) {
-		mongodb.collection('FullLatLngsRecorded').save(
-			{ latlng: LatLng }, function (err, result) {
-				if (err || !result) console.log("latlng not saved");
-				else console.log("latlng loged in DB");
-			});
-	};
+    insertGlobalLeaderboard = function (pos, stats) {
+	var posInt = pos.pos //pos is an array of key value pairs, pos is the key value for the position
+	if (posInt >= 1 && posInt <= 3) {
+	    mongodb.collection('globalLeaderboard').update(
+		{ position: pos }, //'find' query
+		{
+		    position: pos,	//update these (or insert)
+		    date: stats.date,
+		    time: stats.time,
+		    distance: stats.distance,
+		    name: stats.name
+		},
+		{ upsert: true } //insert if not found
+	    )
+	}
+	else {
+	    console.log('invalid pos passed to insertGlobalLeaderboard!');
+	}
+    }
 
-	insertUsername = function (user) {
-		//if (mongodb.collection('UsersSaved').find(user)) console.log("USER IS FOUND IN DB"); Does not work yet. 
-		//else {
+    insertLatLng = function (LatLng) {
+	mongodb.collection('FullLatLngsRecorded').save(
+	    { latlng: LatLng }, function (err, result) {
+		if (err || !result) console.log("latlng not saved");
+		else console.log("latlng loged in DB");
+	    });
+    };
 
-		mongodb.collection('UsersSaved').save(
-			{ users: user }, function (err, result) {
-				if (err || !result) console.log("user not saved");
-				else console.log("user loged in User DB");
-			});
-		//}
-	};
+    checkCreds = function (user) {
+	console.log("checkCreds");
+	results = mongodb.collection('users').find({name:user.name}).toArray();
+	console.log("query success");
+	ret = results && results.length() == 1 && results[0].pass === user.pass;
+	console.log("ret");
+	return ret;
+    };
 
-	printUsers = function (collectionName, callback) {
+    newAccount = function (user){
+	console.log('newAccount');
+	results = mongodb.collection('users').find({name:user.name});
+	if(results.count() != 0) return false;
+	return mongodb.collection('users').insert(user).nInserted == 1 ? true : false;
+    };
 
-		var cursor = mongodb.collection(collectionName).find(function (err, docs) {
-			if (err || !docs) {
-				console.log("Cannot print database or database is empty\n");
-			}
-			else {
-				callback(docs);
-			}
-		});
+    printUsers = function (collectionName, callback) {
 
-	};
+	var cursor = mongodb.collection(collectionName).find(function (err, docs) {
+	    if (err || !docs) {
+		console.log("Cannot print database or database is empty\n");
+	    }
+	    else {
+		callback(docs);
+	    }
+	});
 
-	printLocalLeaderboard = function (collectionName, callback) {
+    };
 
-		var cursor = mongodb.collection(collectionName).find(function (err, docs) {
-			if (err || !docs) {
-				console.log("Cannot print database or database is empty\n");
-			}
-			else {
-				callback(docs);
-			}
-		});
+    printLocalLeaderboard = function (collectionName, callback) {
 
-	};
+	var cursor = mongodb.collection(collectionName).find(function (err, docs) {
+	    if (err || !docs) {
+		console.log("Cannot print database or database is empty\n");
+	    }
+	    else {
+		callback(docs);
+	    }
+	});
 
-	printGlobalLeaderboard = function (collectionName, callback) {
-		
-				var cursor = mongodb.collection(collectionName).find(function (err, docs) {
-					if (err || !docs) {
-						console.log("Cannot print database or database is empty\n");
-					}
-					else {
-						callback(docs);
-					}
-				});
-		
-			};
+    };
 
-	insertPicture = function (pic) {
-		mongodb.collection('PicturesSaved').save(
-			{ pictures: pic }, function (err, result) {
-				if (err || !result) console.log("Picture not saved");
-				else console.log("picture saves in picture DB")
-			});
-	};
+    printGlobalLeaderboard = function (collectionName, callback) {
+	
+	var cursor = mongodb.collection(collectionName).find(function (err, docs) {
+	    if (err || !docs) {
+		console.log("Cannot print database or database is empty\n");
+	    }
+	    else {
+		callback(docs);
+	    }
+	});
+	
+    };
 
-	printPictures = function (collectionName, callback) {
+    insertPicture = function (pic) {
+	mongodb.collection('PicturesSaved').save(
+	    { pictures: pic }, function (err, result) {
+		if (err || !result) console.log("Picture not saved");
+		else console.log("picture saves in picture DB")
+	    });
+    };
 
-		var cursor = mongodb.collection(collectionName).find(function (err, docs) {
+    printPictures = function (collectionName, callback) {
 
-			if (err || !docs) {
-				console.log("Cannot print database or database is empty\n");
-			}
-			else {
-				callback(docs);
-			}
-		});
+	var cursor = mongodb.collection(collectionName).find(function (err, docs) {
 
-	};
+	    if (err || !docs) {
+		console.log("Cannot print database or database is empty\n");
+	    }
+	    else {
+		callback(docs);
+	    }
+	});
 
-	deleteAll = function (colName, callback) {
-		mongodb.collection(colName).remove({});
-		callback(true);
-	};
+    };
 
-	return mongodb;
+    deleteAll = function (colName, callback) {
+	mongodb.collection(colName).remove({});
+	callback(true);
+    };
+
+    return mongodb;
 
 };
