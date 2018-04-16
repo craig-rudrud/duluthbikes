108f6944156c770abfa0e9b4fb57330c1c960e0f
Craig Rudrud <rudru017@d.umn.edu>
2018-04-16 14:56:20 -0500

    Started designing friend layout

--- android/app/src/main/java/com/example/sam/duluthbikes/fragments/FriendsFragment.java
+++ android/app/src/main/java/com/example/sam/duluthbikes/fragments/FriendsFragment.java
@@ -3,27 +3,41 @@ package com.example.sam.duluthbikes.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 
+import com.example.sam.duluthbikes.Friend;
+import com.example.sam.duluthbikes.MyAdapter;
 import com.example.sam.duluthbikes.R;
+
+import java.util.ArrayList;
 
 public class FriendsFragment extends Fragment {
 
-    View myView;
-    EditText mSearchBar;
+    private View myView;
+    private EditText mSearchBar;
+    private RecyclerView mFriendsView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+    private String[] myDataset;
+    private ArrayList<Friend> friendList;
+    private ProgressBar mSearchProgress;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.activity_friends, container, false);
 
-        getFriendsList();
+        friendList = getFriendList();
 
         mSearchBar = myView.findViewById(R.id.friendSearchBar);
         mSearchBar.setOnEditorActionListener(new OnEditorActionListener() {
@@ -31,23 +45,43 @@ public class FriendsFragment extends Fragment {
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
                 boolean handled = false;
                 if(i == EditorInfo.IME_ACTION_SEARCH) {
-                    getFriend(mSearchBar.getText().toString());
-                    handled = true;
+                    Friend friend = getFriend(mSearchBar.getText().toString());
+                    if(friend != null) {
+                        handled = true;
+                    }
                 }
                 return handled;
             }
         });
 
+        mFriendsView = myView.findViewById(R.id.friendsView);
+        mFriendsView.setHasFixedSize(true);
+        mFriendsView.setItemAnimator(new DefaultItemAnimator());
+
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mFriendsView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new MyAdapter(myDataset);
+        mFriendsView.setAdapter(mAdapter);
+
+        mSearchProgress = myView.findViewById(R.id.friendSearchProgress);
+        mSearchProgress.setVisibility(View.INVISIBLE);
+
         return myView;
     }
 
     // TODO: Implement
-    private void getFriend(String friend) {
+    private Friend getFriend(String friendName) {
+        Friend friend = null;
 
+        return friend;
     }
 
-    // TODO: Implement
-    private void getFriendsList() {
+    private ArrayList<Friend> getFriendList() {
+        ArrayList<Friend> list = null;
 
+        //TODO: Get each friend and add them to the list
+
+        return list;
     }
 }
